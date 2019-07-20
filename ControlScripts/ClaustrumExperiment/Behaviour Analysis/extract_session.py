014a1a3435acef86ef8aca56b2b4b24a005c3938
Gao Xiang Ham <gaoxiang90@gmail.com>
2019-07-20 20:30:47 +0100

    Changed parse_sessions to extract all session in a file. Modified print_session_info.

--- ControlScripts/ClaustrumExperiment/Behaviour Analysis/extract_session.py
+++ ControlScripts/ClaustrumExperiment/Behaviour Analysis/extract_session.py
@@ -12,56 +12,81 @@ def main(filename):
         lines = f.read().splitlines()  # reads lines into list
         lines = np.array(
             list(filter(None, lines)))  # removes empty space
-
+        sessions = parse_sessions(lines)
         print_session_info(lines)
-        s_type = '4'  # change number based on desired session
-        session = parse_session(lines, s_type)
-        print(session)
-#    info = extract_data(sessions[i], "D:", "E:")
-#    print(info)
+
+#    s_type = '4'  # change number based on desired session type
+#    session = parse_session_type(lines, s_type)
+    
+    s_index = 1  # change number based on desired session
+    data = extract_data(sessions[s_index], "L:", "M:")
+    print(data)
+
 
 
 def print_session_info(lines):
     s_list = np.flatnonzero(
             np.core.defchararray.find(lines, "MSN:") != -1)
+    # returns index in np.array for cells containing "MSN:"
     a_list = np.flatnonzero(
             np.core.defchararray.find(lines, "Subject:") != -1)
-    print('Sessions in file:', lines[np.stack((a_list, s_list), axis=-1)])
+    p_list = np.stack((a_list, s_list), axis=-1)
+    i = 0
+    print('Sessions in file:')
+    for a, s in p_list:
+        print(i, '->', lines[a], ',', lines[s])
+        i += 1
+    return print('')
 
 
-def parse_session(lines, s_type):
-    if s_type == '2':
-        s_type = 'MSN: 2_MagazineHabituation_p'
-    elif s_type == '3':
-        s_type = 'MSN: 3_LeverHabituation_p'
-    elif s_type == '4':
-        s_type = 'MSN: 4_LeverTraining_p'
-    elif s_type == '5a':
-        s_type = 'MSN: 5a_FixedRatio_p'
-    elif s_type == '5b':
-        s_type = 'MSN: 5b_FixedInterval_p'
-    elif s_type == 'DNMTS':
-        s_type = 'MSN: DNMTS'
-    else:
-        print('Error! Invalid session type!')
-        return None
-
-    id_increment = 8
+def parse_sessions(lines):
     s_starts = np.flatnonzero(
         np.core.defchararray.find(lines, "Start Date:") != -1)
     s_ends = np.zeros_like(s_starts)
     s_ends[:-1] = s_starts[1:]
     s_ends[-1] = lines.size
-    s_identifiers = lines[s_starts + id_increment]
-    s_id_indices = np.nonzero(s_identifiers == s_type)
-    s_id_starts = s_starts[s_id_indices]
-    s_id_ends = s_ends[s_id_indices]
-
     sessions = []
-    for start, end in zip(s_id_starts, s_id_ends):
-        s_data = lines[start:end]
+    for start, end in zip(s_starts, s_ends):
+        s_data = np.array(lines[start:end])
+        print(len(s_data))
         sessions.append(s_data)
     return sessions
+
+
+#def parse_session_type(lines, s_type):
+#    #  Extracts specific data from specific session types
+#    if s_type == '2':
+#        s_type = 'MSN: 2_MagazineHabituation_p'
+#    elif s_type == '3':
+#        s_type = 'MSN: 3_LeverHabituation_p'
+#    elif s_type == '4':
+#        s_type = 'MSN: 4_LeverTraining_p'
+#    elif s_type == '5a':
+#        s_type = 'MSN: 5a_FixedRatio_p'
+#    elif s_type == '5b':
+#        s_type = 'MSN: 5b_FixedInterval_p'
+#    elif s_type == 'DNMTS':
+#        s_type = 'MSN: DNMTS'
+#    else:
+#        print('Error! Invalid session type!')
+#        return None
+#
+#    id_increment = 8
+#    s_starts = np.flatnonzero(
+#        np.core.defchararray.find(lines, "Start Date:") != -1)
+#    s_ends = np.zeros_like(s_starts)
+#    s_ends[:-1] = s_starts[1:]
+#    s_ends[-1] = lines.size
+#    s_identifiers = lines[s_starts + id_increment]
+#    s_id_indices = np.nonzero(s_identifiers == s_type)
+#    s_id_starts = s_starts[s_id_indices]
+#    s_id_ends = s_ends[s_id_indices]
+#
+#    sessions = []
+#    for start, end in zip(s_id_starts, s_id_ends):
+#        s_data = np.array(lines[start:end])
+#        sessions.append(s_data)
+#    return sessions
 
 
 def extract_data(lines, start_char, end_char):
@@ -80,7 +105,8 @@ def extract_data(lines, start_char, end_char):
             st = 5 * i
             arr[st:st + len(numbers)] = numbers
         data_list.append(arr)
-    return data_list
+#        print(type(data_list))
+    return data_list[0]
 
 
 def parse_line(line, dtype=np.float32):
