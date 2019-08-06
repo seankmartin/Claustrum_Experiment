f4681b1539b5803d9e14555a0e3b5613d5108b69
Sean Martin <martins7@tcd.ie>
2019-08-06 14:14:06 +0100

    Added initial version of exp 7

--- ClaustrumExperiment/convert_all.bat
+++ ClaustrumExperiment/convert_all.bat
@@ -2,7 +2,7 @@
 
 set ROOT=C:\Users\smartin5\Repos\neuro-tools\
 set LOCA=%ROOT%\ClaustrumExperiment\MEDPC
-set SCRIPTLOC=%ROOT%\parse_mpc.py
+set SCRIPTLOC=%ROOT%\ClaustrumExperiment\parse_mpc.py
 
 echo "Working in %ROOT%"
 for %%f in (%LOCA%\*.pmpc) do (
