8ca0f3ffd2e8292728c70b6dc14d7dfc80f63753
GxHam <gaoxiang90@gmail.com>
2019-08-13 18:14:08 +0100

    Added annotation of stage and modifications to timeline.

--- ClaustrumExperiment/bvmpc/bv_control.py
+++ ClaustrumExperiment/bvmpc/bv_control.py
@@ -7,16 +7,23 @@ import bv_analyse as bv_an
 from bv_utils import make_dir_if_not_exists, print_h5, mycolors
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
+from adjustText import adjust_text
+from scipy import interpolate
 
 
-def plot_sessions(summary=False, single=True, timeline=False):
+def plot_sessions(summary=False, single=True, timeline=True):
+    ''' Plots session summaries
+    summary = True: Plots all sessions in a single plot, up to 6
+    single = True: Plots single session summaries with breakdown of single blocks
+    Timeline = True: Plots total rewards from beginining of first session
+    '''
     # Parameters for specifying session
     sub_list = ['1', '2', '3', '4', '5', '6']
 #    sub_list = ['6']
 #    sub_list = ['1', '2', '3', '4']
 #    sub_list = ['5', '6']
-    s_list = ['6', '7']
-    d_list = ['08-07']
+    s_list = ['7']
+    d_list = ['08-10']
     in_dir = r"F:\PhD (Shane O'Mara)\Operant Data\IR Discrimination Pilot 1\hdf5"
     out_dir = r"F:\PhD (Shane O'Mara)\Operant Data\IR Discrimination Pilot 1\Plots"
 
@@ -30,7 +37,7 @@ def plot_sessions(summary=False, single=True, timeline=False):
             sum_plot(s_grp2, out_name+'_2', out_dir, IRT=False)        
         else:
             sum_plot(s_grp, out_name, out_dir, IRT=False)
-    elif single:
+    if single and summary:
         # Single Subject Plots
         for c, sub in enumerate(sub_list):
             s_grp, out_name = extract_hdf5s(in_dir, out_dir, sub, s_list, d_list)
@@ -44,8 +51,10 @@ def plot_sessions(summary=False, single=True, timeline=False):
                 rows, cols = [len(s_grp), 4]
                 fig = plt.figure(
                         figsize=(cols * size_multiplier,
-                                 rows * size_multiplier), tight_layout=False)
+                                 rows * size_multiplier), tight_layout=True)
                 gs = gridspec.GridSpec(rows, cols, wspace=0.3, hspace=0.3)
+                if len(s_grp) == 1:
+                    fig.tight_layout(rect=[0, 0.03, 0.8, 0.95])
                 for i, s in enumerate(s_grp):
                     ax1 = fig.add_subplot(gs[i, 0])
                     bv_an.cumplot(s, out_dir, ax1, zoom=False, zoom_sch=False)
@@ -58,7 +67,12 @@ def plot_sessions(summary=False, single=True, timeline=False):
                     ax4 = fig.add_subplot(gs[i, 3])
                     bv_an.cumplot(s, out_dir, ax4, zoom=False, zoom_sch=True,
                               plot_error=True, plot_all=False)
+#                if len(s_grp) == 1:
+#                    fig.suptitle(('Subject ' + subject + ' Performance'), y = 1.05, fontsize=30)
+#                else:
+                plt.subplots_adjust(top=0.85)
                 fig.suptitle(('Subject ' + subject + ' Performance'), fontsize=30)
+
 #                # Seperate plots w line
 #                ax1.hlines(1.13, -0, 4.9, clip_on=False,
 #                           transform=ax1.transAxes, linewidth=0.7)
@@ -135,22 +149,58 @@ def timeline_plot(sub_list, in_dir, out_dir, single_plot=False):
         s_grp, out_name = extract_hdf5s(in_dir, out_dir, sub)
         s_list = []
         r_list = []
+        changes = []
+        stage_change = []
+        change_idx = []
+        prev_ratio = []
+        prev_interval = []
+        c_ratio = []
+        c_interval = []
+        type_list = []
+        prev_name = '2'
         for i, s in enumerate(s_grp):
             s_type = s.get_metadata('name')[:2]
             subject = s.get_metadata('subject')
             timestamps = s.get_arrays()
             if s_type == '5a':
                 s_name = 'R' + str(int(timestamps["Experiment Variables"][3]))
+                c_ratio = s_name
             elif s_type == '5b':
                 s_name = 'I' + str(int(timestamps[
                         "Experiment Variables"][3]/100))
+                c_interval = s_name
             else:
                 s_name = s_type.replace('_', '').replace('2', 'M').replace(
                         '3', 'Lh').replace('4', 'Lt').replace(
-                                '6', 'R1').replace('7', 'R2')
+                                '6', 'B1').replace('7', 'B2')
+            if 'B' in s_name:
+                c_ratio = 'R' + str(int(timestamps["Experiment Variables"][3]))
+                c_interval = 'I' + str(int(timestamps["Experiment Variables"][5] / 100))
+            if not prev_name[0] == s_type[0]:
+                stage_change.append(1)
+                changes.append(0)
+                change_idx.append(0)
+            else:
+                stage_change.append(0)
+                if not c_ratio == prev_ratio and not c_interval == prev_interval:
+                    changes.append([c_ratio, c_interval])
+                    change_idx.append(1)
+                elif not c_ratio == prev_ratio:
+                    changes.append(c_ratio)
+                    change_idx.append(1)
+                elif not c_interval == prev_interval:
+                    changes.append(c_interval)
+                    change_idx.append(1)
+                else:
+                    changes.append(0)
+                    change_idx.append(0)
             rewards_t = len(timestamps["Reward"])
             s_list.append(s_name)
             r_list.append(rewards_t)
+            type_list.append('Stage '+s_type[0])
+            prev_ratio = c_ratio
+            prev_interval = c_interval
+            prev_name = s_type
         s_idx = np.arange(0, len(s_list))
         if single_plot:
             rows, cols = [1, 4]
@@ -164,9 +214,30 @@ def timeline_plot(sub_list, in_dir, out_dir, single_plot=False):
         else:
             ax = fig.add_subplot(gs[int(c), :])
 
-        plt.plot(s_idx, r_list, label='Animal'+subject, linewidth='4',
+        h1, = plt.plot(s_idx, r_list, label='Animal'+subject, linewidth='4',
                  color=mycolors(subject))
-        plt.xticks(s_idx, s_list, fontsize=15)
+        
+#        # Testing alterative annotation
+#        texts = []
+#        for stage, x, y, s in zip(stage_change, s_idx, r_list, type_list):
+#            if stage == 1:
+#                texts.append(plt.text(x, y, s))
+#        f = interpolate.interp1d(s_idx, r_list)
+#        x = np.arange(min(s_idx), max(s_idx), 0.0005)
+#        y = f(x)  
+#        adjust_text(texts, x=x, y=y, only_move={'points':'xy', 'text':'xy'}, 
+#                    autoalign='xy', force_points=0.5, arrowprops=dict(arrowstyle="->", color='r', lw=0.5))
+
+        # Annotated changes in protocol
+        for i, c in enumerate(changes):
+            if stage_change[i] == 1:
+                h2 = ax.annotate(type_list[i], xy=(s_idx[i], r_list[i]), xytext=(s_idx[i], r_list[i]+(0.1*max(r_list))),
+                            arrowprops=dict(facecolor='blue', shrink=0.05))
+            elif change_idx[i] == 1:
+                h3 = ax.annotate(str(c), xy=(s_idx[i], r_list[i]), xytext=(s_idx[i], r_list[i]+(0.1*max(r_list))),
+                            arrowprops=dict(facecolor='Red', shrink=0.05))
+        ax.set_xlim(0, len(s_idx))
+        plt.xticks(s_idx, s_list, fontsize=13)
         ax.tick_params(axis='y', labelsize=15)
         plt.axhline(45, color='g', linestyle='-.', linewidth='.5')
         plt.axhline(90, color='r', linestyle='-.', linewidth='.5')
@@ -174,7 +245,7 @@ def timeline_plot(sub_list, in_dir, out_dir, single_plot=False):
         ax.spines['right'].set_visible(False)
         ax.set_xlabel('Sessions', fontsize=20)
         ax.set_ylabel('Total Rewards', fontsize=20)
-        plt.legend()
+        plt.legend([h1, h2.arrow_patch, h3.arrow_patch], (h1.get_label(), 'Stage Changes', 'Protocol Modification'))
         ax.set_title('\nSubject {} Timeline'.format(subject), fontsize=25)
         
         if single_plot:
@@ -299,7 +370,7 @@ if __name__ == "__main__":
 #    out_dir = r"G:\out_plots"
 #    out_dir = r"/home/sean/Documents/Data/results"
     
-#    filename = r"F:\PhD (Shane O'Mara)\Operant Data\IR Discrimination Pilot 1\!2019-08-08"
+#    filename = r"F:\PhD (Shane O'Mara)\Operant Data\IR Discrimination Pilot 1\!2019-08-12"
 #    out_dir = r"F:\PhD (Shane O'Mara)\Operant Data\IR Discrimination Pilot 1\hdf5"
 #    convert_to_hdf5(filename, out_dir)  # Uncomment to convert to hdf5
 #    run_mpc_file(filename, out_dir)
