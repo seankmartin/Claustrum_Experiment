e3398f2d2a2f77dbc3df6e82889745a38adabf3c
Sean Martin <martins7@tcd.ie>
2019-11-18 21:24:42 +0000

    Refactored UI

--- temp/bin_count_ui.py
+++ temp/bin_count_ui.py
@@ -1,14 +1,37 @@
+import os
 from bin_count import run_mpc_file
 
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication
 
-Form, Window = uic.loadUiType("MedPC_Split.ui")
 
-app = QApplication([])
-window = Window()
-form = Form()
-form.setupUi(window)
-# NEED TO FIGURE OUT print(app.get_widgets())
-window.show()
-app.exec_()
+class DesignerUI:
+    def __init__(self, design_location):
+        Form, Window = uic.loadUiType(design_location)
+        self.app = QApplication([])
+        self.window = Window()
+        self.ui = Form()
+        self.ui.setupUi(self.window)
+
+    def start(self):
+        self.window.show()
+        self.app.exec_()
+
+    def get_widgets(self):
+        return self.app.allWidgets()
+
+    def get_widget_names(self):
+        return [w.objectName() for w in self.get_widgets()]
+
+
+class BinCountUI(DesignerUI):
+    def selectFile(self):
+        print(self.ui.FileSelectButton)
+
+
+if __name__ == "__main__":
+    here = os.path.dirname(os.path.realpath(__file__))
+    ui_location = os.path.join(
+        here, "MedPC_Split.ui")
+    ui = BinCountUI(ui_location)
+    ui.start()
