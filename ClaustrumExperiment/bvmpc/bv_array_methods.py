56ddada01bd05c9b970a3116b3904a3205746ac5
Sean Martin <martins7@tcd.ie>
2019-11-20 23:52:03 +0000

    Added another array helper method

--- ClaustrumExperiment/bvmpc/bv_array_methods.py
+++ ClaustrumExperiment/bvmpc/bv_array_methods.py
@@ -21,6 +21,8 @@ def split_array(array, idxs):
     (array[idxs], array[not idxs])
 
     """
+    if len(idxs) == 0:
+        return [], []
     left = array[idxs]
     ia = np.indices(array.shape)
     not_indices = np.setxor1d(ia, idxs)
@@ -31,7 +33,7 @@ def split_array(array, idxs):
 
 def split_array_with_another(array, split_arr):
     """
-    Split a sorted one darray into two with 
+    Split a sorted one d array into two with 
     one array being the values directly after splt_arr.
 
     Returns
@@ -43,11 +45,33 @@ def split_array_with_another(array, split_arr):
     return split_array(array, idxs)
 
 
+def split_array_in_between_two(array, left, right):
+    """
+    Split a sorted one d array into two with
+    one array being the values in between left and right
+    left and right should be the same size
+    """
+    if len(left) != len(right):
+        print(
+            "ERROR! left and right should have the same size in" +
+            " split_array_in_between_two")
+        exit(-1)
+    good_idxs = []
+    for i, val in enumerate(array):
+        bigger = (left <= val)
+        smaller = (right >= val)
+        between = np.logical_and(smaller, bigger)
+        if between.any():
+            good_idxs.append(i)
+    return split_array(array, good_idxs)
+
+
 if __name__ == "__main__":
     array = np.array([i + 1 for i in range(9)])
     tests = []
     tests.append(split_into_blocks(array, 3, 3))
     tests.append(split_array(array, [0, 1, 2]))
     tests.append(split_array_with_another(array, [0.9, 3.9, 8.9]))
+    tests.append(split_array_in_between_two(array, [1, 8], [4, 9]))
     for t in tests:
         print(t)
