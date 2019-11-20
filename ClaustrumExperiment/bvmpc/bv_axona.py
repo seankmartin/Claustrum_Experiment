d135c09163de34802688233d243ca95461e329d8
Sean Martin <martins7@tcd.ie>
2019-11-20 18:16:11 +0000

    Added time retrieval function to bv_axona

--- ClaustrumExperiment/bvmpc/bv_axona.py
+++ ClaustrumExperiment/bvmpc/bv_axona.py
@@ -84,6 +84,34 @@ class AxonaInput:
             np.unpackbits(p_arr[2 * i:2 * i + 2])[::-1]
             for i in range(len(p_arr) // 2)])
 
+    def get_times(self, channel, pin, inverted=False):
+        """
+        Get timestamps of pin changes on a channel.
+
+        Parameters
+        ----------
+        channel: str
+            Which channel to get
+        pin: int
+            Which pin to retrieve, 1 indexed
+        inverted: bool - default False
+            if True, 1 is off and 0 is on
+        """
+        if inverted:
+            on = 0
+        else:
+            on = 1
+        good_idx = np.nonzero(self.channels == channel)
+        times = self.timestamps[good_idx]
+        pins = self.pins[good_idx]
+        pins = pins[:, pin - 1].flatten()
+        differences = np.diff(pins)
+        change_idxs = np.add(np.nonzero(differences), 1).flatten()
+        pin_values_at_change = pins[change_idxs]
+        times_at_change = times[change_idxs]
+        on_change_idxs = np.nonzero(pin_values_at_change == on)
+        return times_at_change[on_change_idxs]
+
 
 class AxonaInpReader:
     """Axona .inp reader - this class is completely static"""
@@ -103,8 +131,8 @@ class AxonaInpReader:
         Returns
         -------
         None
-        """
 
+        """
         counter = 0
         with open(in_location, 'rb') as file:
             header = cls._parse_header(file)
@@ -166,6 +194,8 @@ class AxonaInpReader:
 def main(in_location):
     ai = AxonaInput(in_location)
     ai.save_to_file()
+    times = ai.get_times("I", 1, True)
+    print(times)
 
 
 def main_cmd():
