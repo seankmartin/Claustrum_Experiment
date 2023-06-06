7205a7ef88fa11297116892eadb49051eb4b086f
Sean Martin <seankieran.m@hotmail.com>
2023-06-06 12:27:46 +0100

    Plot updates to include estimated

--- workflow/scripts/plot_coherence.py
+++ workflow/scripts/plot_coherence.py
@@ -9,9 +9,6 @@ import seaborn as sns
 import simuran as smr
 from skm_pyutils.table import df_from_file, list_to_df
 
-TO_PLOT = "Trial type"
-
-
 def convert_to_long_form(df, max_frequency=40):
     new_res = []
     columns = df.columns
@@ -90,8 +87,8 @@ def plot_coherence(df, out_dir, max_frequency=40):
             data=df[df["Brain regions"] == r],
             x="Frequency (Hz)",
             y="Coherence",
-            style=TO_PLOT,
-            hue=TO_PLOT,
+            style="Trial type",
+            hue="Trial type",
             estimator="median",
             # estimator="mean",
             errorbar=("ci", 95),
@@ -146,44 +143,46 @@ def plot_band_coherence(input_df, output_dir):
     ]
     new_df = list_to_df(new_list, headers)
 
-    for br in ["CLA RSC", "CLA ACC", "RSC ACC"]:
-        sub_df = new_df[new_df["Brain regions"] == br]
-        fig, ax = plt.subplots(figsize=(10, 8))
-        sns.boxplot(
-            data=sub_df,
-            y="Coherence",
-            x="Band",
-            hue=TO_PLOT,
-            palette="pastel",
-            ax=ax,
-            showfliers=False,
-            width=0.9,
-            # palette=["0.65", "r"],
-        )
-        sns.stripplot(
-            data=sub_df,
-            y="Coherence",
-            x="Band",
-            hue=TO_PLOT,
-            ax=ax,
-            # palette="dark:grey",
-            palette=["0.4", "0.75"],
-            alpha=0.95,
-            dodge=True,
-            edgecolor="k",
-            linewidth=1,
-            size=4.5,
-            legend=False,
-        )
-        smr.despine()
-        smr_fig = smr.SimuranFigure(fig, output_dir / f"coherence_{br}_")
-        smr_fig.save()
+    for to_plot, n in zip(["Trial type", "Estimated trial type"], ["trial", "estimated"]):
+        for br in ["CLA RSC", "CLA ACC", "RSC ACC"]:
+            sub_df = new_df[new_df["Brain regions"] == br]
+            fig, ax = plt.subplots(figsize=(10, 8))
+            sns.boxplot(
+                data=sub_df,
+                y="Coherence",
+                x="Band",
+                hue=to_plot,
+                palette="pastel",
+                ax=ax,
+                showfliers=False,
+                width=0.9,
+                # palette=["0.65", "r"],
+            )
+            sns.stripplot(
+                data=sub_df,
+                y="Coherence",
+                x="Band",
+                hue=to_plot,
+                ax=ax,
+                # palette="dark:grey",
+                palette=["0.4", "0.75"],
+                alpha=0.95,
+                dodge=True,
+                edgecolor="k",
+                linewidth=1,
+                size=4.5,
+                legend=False,
+            )
+            smr.despine()
+            smr_fig = smr.SimuranFigure(fig, output_dir / f"coherence_{br}_{n}")
+            smr_fig.save()
 
 
 def main(input_df_path, out_dir, config_path):
     config = smr.config_from_file(config_path)
     coherence_df = df_from_file(input_df_path)
     long_df = convert_to_long_form(coherence_df, config["max_psd_freq"])
+    long_df = long_df[long_df["Estimated trial type"] != "Fail"]
     fix_notch_freqs(long_df, config["notch_freqs"])
     plot_coherence(long_df, out_dir, config["max_psd_freq"])
     plot_band_coherence(coherence_df, out_dir)
